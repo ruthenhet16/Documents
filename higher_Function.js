9b7a60e6b9759daae69ac9802978c5252276be10
Ruth Elizabeth Nhet <89280001+ruthenhet16@users.noreply.github.com>
2021-09-14 10:04:19 -0700

    Add files via upload

---
+++ higher_Function.js
@@ -0,0 +1,41 @@
+const checkThatTwoPlusTwoEqualsFourAMillionTimes = () => {
+  for(let i = 1; i <= 1000000; i++) {
+    if ( (2 + 2) != 4) {
+      console.log('Something has gone very wrong :( ');
+    }
+  }
+};
+
+// Write your code below
+
+ //const is2p2 = checkThatTwoPlusTwoEqualsFourAMillionTimes;
+ //is2p2(); // involke this function
+ //console.log(is2p2.name);
+
+ // function as parameters
+
+
+const addTwo = num => num + 2;
+
+const timeFuncRuntime = funcParameter => {
+  let t1 = Date.now();
+  funcParameter();
+  let t2 = Date.now();
+  return t2 - t1;
+};
+
+// Write your code below
+
+const time2p2 = timeFuncRuntime(checkThatTwoPlusTwoEqualsFourAMillionTimes);
+
+const checkConsistentOutput = (func, val) => {
+    let firstTry = func(val);
+    let secondTry = func(val);
+    if (firstTry === secondTry) {
+        return firstTry
+    } else {
+        return 'This function returned inconsistent results'
+    }
+};
+
+checkConsistentOutput(addTwo, 10);
